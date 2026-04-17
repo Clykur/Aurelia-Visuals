af065edc8321664b1d9411421f4c26cfd030993c
chinnuk0521 <chinnuk0521@gmail.com>
2026-04-17 21:41:36 +0530

    Name chnage

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,27 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import type { IncomingMessage, ServerResponse } from "node:http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
+type PinoSerializedReq = IncomingMessage & { id?: string };
+type PinoSerializedRes = ServerResponse;
+
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: PinoSerializedReq) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: PinoSerializedRes) {
         return {
           statusCode: res.statusCode,
         };
